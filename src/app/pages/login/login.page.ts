bfb901128e84e2e55e76c7e16df7569351716352
maxi2 <maximilianoc618@gmail.com>
2023-11-01 22:29:00 -0300

    subiendo login

---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  mdl_usuario : string = '';
+  mdl_contrasena : string = '';
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+
+  ingresar() {
+    
+  }
+
+}
